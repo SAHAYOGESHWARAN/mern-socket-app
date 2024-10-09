eb7c94ba5e7aa1dba9d4852444b5b66cc83e6b4c
SAHAYOGESHWARAN <sahayogeshwaran1@gmail.com>
2024-10-09 10:23:06 +0530

    mern

---
+++ client/src/components/App.js
@@ -0,0 +1,15 @@
+import React from 'react';
+import Chat from './Chat';
+import DocumentEditor from './DocumentEditor';
+
+function App() {
+  return (
+    <div>
+      <h1>Workspace Collaboration Tool</h1>
+      <Chat />
+      <DocumentEditor />
+    </div>
+  );
+}
+
+export default App;
